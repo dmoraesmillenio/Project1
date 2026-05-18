02c392ac37fea17875da2b8e546f0f336b933612
Deusimar Moraes <deusimar@millenio.com.br>
2026-05-18 14:01:18 -0300

    Primeiro commit PHPRunner

---
+++ connections/databases.php
@@ -0,0 +1,28 @@
+<?php
+global $runnerDatabases;
+$runnerDatabases = array(
+	'conn' => array(
+		'connId' => 'conn',
+		'connName' => 'sakila at localhost',
+		'dbType' => 4,
+		'connStringType' => 'postgre',
+		'connInfo' => array( 
+			'localhost',
+			'phprunner',
+			'phprunner',
+			'14297',
+			'sakila',
+			'',
+			'1',
+			'{}',
+			'0' 
+		) 
+	) 
+);
+
+global $runnerRestConnections;
+$runnerRestConnections = array(
+	 
+);
+
+?>
---
+++ settings/dbtables.php
@@ -0,0 +1,11 @@
+<?php
+global $runnerDbTables;
+$runnerDbTables = array( 
+	array(
+		'connId' => 'conn',
+		'table' => 'actor',
+		'schema' => 'public',
+		'filename' => 'public_actor' 
+	) 
+);
+?>
