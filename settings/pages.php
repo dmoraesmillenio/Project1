c7d2722b6bc79b5b197db00067a35e9775781b05
Deusimar Moraes <deusimar@millenio.com.br>
2026-05-19 13:07:32 -0300

    Atualização PHPRunner

---
+++ settings/pages.php
@@ -0,0 +1,56 @@
+<?php
+$runnerPageInfo = array(
+	'allPages' => array(
+		'public.actor' => array(
+			'add' => array( 
+				'add' 
+			),
+			'export' => array( 
+				'export' 
+			),
+			'import' => array( 
+				'import' 
+			),
+			'edit' => array( 
+				'edit' 
+			),
+			'view' => array( 
+				'view' 
+			),
+			'list' => array( 
+				'list' 
+			),
+			'print' => array( 
+				'print' 
+			) 
+		),
+		'<global>' => array(
+			'menu' => array( 
+				'menu' 
+			) 
+		),
+		'public.actor_info' => array(
+			'add' => array( 
+				'add' 
+			),
+			'export' => array( 
+				'export' 
+			),
+			'import' => array( 
+				'import' 
+			),
+			'list' => array( 
+				'list' 
+			),
+			'print' => array( 
+				'print' 
+			) 
+		) 
+	),
+	'tableMasks' => array(
+		'public.actor' => 'ADESPI',
+		'public.actor_info' => 'ADSPI',
+		'<global>' => 'S' 
+	) 
+);
+?>
---
+++ settings/project.php
@@ -0,0 +1,246 @@
+<?php
+$runnerProjectSettings = array(
+	'restAPIReturnEncodedBinary' => true,
+	'restAPIAuthType' => 'basic',
+	'menuIds' => array( 
+		'main' 
+	),
+	'tablesAdvSecurity' => array(
+		'public.actor' => array(
+			'table' => 2527 
+		),
+		'public.actor_info' => array(
+			'table' => 2562 
+		) 
+	),
+	'phpSpreadsheet' => false,
+	'ext' => 'php',
+	'security' => array(
+		'projectName' => '',
+		'loginDataSource' => '',
+		'loginForm' => 3,
+		'dynamicPermissions' => false,
+		'dpTablePrefix' => '',
+		'dpTableConnId' => '',
+		'providers' => array( 
+			 
+		),
+		'enabled' => false,
+		'advancedSecurityAvailable' => false,
+		'userGroupsAvailable' => false,
+		'hardcodedLogin' => false,
+		'defaultProviderCode' => '',
+		'adOnlyLogin' => false,
+		'sessionControl' => array(
+			'lifeTime' => 15,
+			'sessionName' => 'HU5XPz4ZT2YE95iarIlq',
+			'JWTSecret' => 'dbtACCbuacSdU2FVdp1o' 
+		),
+		'registration' => array(
+			'remindMethod' => 0,
+			'hashAlgorithm' => 0,
+			'passwordValidation' => array(
+				'strong' => false,
+				'minimumLength' => 8,
+				'uniqueCharacters' => 4,
+				'digitsAndSymbols' => 2,
+				'upperAndLowerCase' => false 
+			),
+			'registerPage' => false 
+		),
+		'captchaSettings' => array(
+			'captchaType' => 0,
+			'siteKey' => '',
+			'secretKey' => '',
+			'passesCount' => 5 
+		),
+		'emailSettings' => array(
+			'fromEmail' => '',
+			'usePHPDefinedSMTP' => false,
+			'useBuiltInMailer' => false,
+			'SMTPServer' => 'localhost',
+			'SMTPPort' => 25,
+			'SMTPUser' => '',
+			'SMTPPassword' => '',
+			'securityProtocol' => 0,
+			'provider' => 0,
+			'oauthUserEmail' => '',
+			'oauthSettingsJson' => '',
+			'oauthClientId' => '',
+			'oauthClientSecret' => '',
+			'oauthTenantId' => '' 
+		),
+		'advancedSecurity' => array(
+			'allowGuestLogin' => false 
+		),
+		'auditAndLocking' => array(
+			'loggingMode' => 0,
+			'loggingTable' => array(
+				'connId' => '',
+				'table' => '' 
+			),
+			'loggingFile' => 'audit.log',
+			'logSecurityActions' => false,
+			'lockAfterUnsuccessfulLogin' => false,
+			'enableLocking' => false,
+			'lockingTable' => array(
+				'connId' => '',
+				'table' => '' 
+			),
+			'tables' => array(
+				 
+			) 
+		),
+		'twoFactorSettings' => array(
+			'available' => false,
+			'required' => false,
+			'enable' => true,
+			'remember' => true,
+			'types' => array(
+				 
+			),
+			'twoFactorField' => '',
+			'emailField' => '',
+			'phoneField' => '',
+			'codeField' => '',
+			'projectName' => '' 
+		),
+		'staticPermissions' => array(
+			'groups' => array(
+				 
+			) 
+		),
+		'adAdminGroups' => array( 
+			 
+		),
+		'showUserSource' => false,
+		'dbProviderCodes' => array( 
+			 
+		) 
+	),
+	'notifications' => array(
+		'enabled' => false,
+		'table' => array(
+			'connId' => '',
+			'table' => '' 
+		) 
+	),
+	'allTables' => array(
+		'public.actor' => array(
+			'gid' => 2527,
+			'name' => 'public.actor',
+			'shortName' => 'actor',
+			'type' => 0,
+			'caption' => array(
+				'English' => 'Actor' 
+			),
+			'connId' => 'conn',
+			'color' => 'e67349',
+			'originalTable' => 'public.actor' 
+		),
+		'public.actor_info' => array(
+			'gid' => 2562,
+			'name' => 'public.actor_info',
+			'shortName' => 'actor_info',
+			'type' => 0,
+			'caption' => array(
+				'Portuguese(Brazil)' => 'Actor Info' 
+			),
+			'connId' => 'conn',
+			'color' => 'cd853f',
+			'originalTable' => 'public.actor_info' 
+		) 
+	),
+	'tablesByShort' => array(
+		'actor' => 'public.actor',
+		'actor_info' => 'public.actor_info' 
+	),
+	'tablesByGood' => array(
+		'public_actor' => 'public.actor',
+		'public_actor_info' => 'public.actor_info' 
+	),
+	'events' => array( 
+		 
+	),
+	'languages' => array( 
+		array(
+			'name' => 'Portuguese(Brazil)',
+			'nativeName' => 'Português(Brasil)',
+			'rtl' => false,
+			'filename' => 'Portuguese.lng' 
+		) 
+	),
+	'languageNames' => array( 
+		'Portuguese(Brazil)' 
+	),
+	'defaultLanguage' => 'Portuguese(Brazil)',
+	'detectDefaultLanguage' => true,
+	'charset' => 'utf-8',
+	'codepage' => 65001,
+	'defaultConnID' => 'conn',
+	'wrConnectionID' => '',
+	'wizardBuild' => '44307',
+	'projectBuild' => 'plNRHkgsaV6l',
+	'projectTheme' => 'default',
+	'projectSize' => 'normal',
+	'customErrorMsg' => array(
+		'text' => 'Error occured.',
+		'type' => 0 
+	),
+	'cloudSettings' => array(
+		'cloudAmazonRegion' => '',
+		'cloudAmazonBucket' => '',
+		'cloudAmazonAccessKey' => '',
+		'cloudAmazonSecretKey' => '',
+		'cloudWasabiRegion' => '',
+		'cloudWasabiBucket' => '',
+		'cloudWasabiAccessKey' => '',
+		'cloudWasabiSecretKey' => '',
+		'cloudGDriveClientId' => '',
+		'cloudGDriveClientSecret' => '',
+		'cloudOneDriveClientId' => '',
+		'cloudOneDriveClientSecret' => '',
+		'cloudOneDriveDrive' => '',
+		'cloudOneDriveAccountType' => 0,
+		'cloudOneDriveDirectoryId' => '',
+		'cloudDropboxClientId' => '',
+		'cloudDropboxClientSecret' => '',
+		'cloudGoogleCloudAccessKey' => '',
+		'cloudGoogleCloudSecret' => '',
+		'cloudGoogleCloudBucket' => '' 
+	),
+	'mapSettings' => array(
+		'embed' => true,
+		'provider' => 0,
+		'apikey' => '' 
+	),
+	'viewPluginsWithJS' => array( 
+		 
+	),
+	'rtlLanguages' => array(
+		'Portuguese(Brazil)' => false 
+	),
+	'smsSettings' => array(
+		'smsProvider' => 4,
+		'iBusername' => '',
+		'iBpassword' => '',
+		'iBsender' => '',
+		'essUsername' => '',
+		'essPassword' => '',
+		'essSender' => '',
+		'gwApiToken' => '',
+		'gwSender' => '',
+		'mbAuth' => '',
+		'mbSender' => '',
+		'twilioSID' => '',
+		'twilioAuth' => '',
+		'twilioNumber' => '',
+		'phoneField' => '',
+		'counryCode' => '+1',
+		'wauUsername' => '',
+		'wauPassword' => '',
+		'wauSender' => '' 
+	) 
+);
+
+?>
