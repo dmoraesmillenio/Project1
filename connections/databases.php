c2e6980e83722d1d9d34044fb38021c1194ce522
Deusimar Moraes <deusimar@millenio.com.br>
2026-05-20 09:57:33 -0300

    Atualização PHPRunner

---
+++ connections/databases.php
@@ -0,0 +1,28 @@
+<?php
+global $runnerDatabases;
+$runnerDatabases = array(
+	'conn' => array(
+		'connId' => 'conn',
+		'connName' => 'sakila at localhost',
+		'dbType' => 4,
+		'connStringType' => 'postgre',
+		'connInfo' => array( 
+			'localhost',
+			'phprunner',
+			'phprunner',
+			'14297',
+			'sakila',
+			'',
+			'1',
+			'{"ssl":false}',
+			'0' 
+		) 
+	) 
+);
+
+global $runnerRestConnections;
+$runnerRestConnections = array(
+	 
+);
+
+?>
